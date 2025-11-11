a1a6f46d7d49933e189b17fc537035c10582c9f6
Vessely1 <nv.veselyi@ukma.edu.ua>
2025-11-11 22:43:58 +0200

    openapi docs

--- backend/kyking_project/urls.py
+++ backend/kyking_project/urls.py
@@ -16,8 +16,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="KyKing",
+      default_version='v1',
+      description="API documentation for KyKing",
+      terms_of_service="https://kyking.com/terms/",
+      contact=openapi.Contact(email="team@kyking.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('recipes_app.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
