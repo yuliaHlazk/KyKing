062845fa42fce82bcd3277f5ac9b40b00f9288fc
Vessely1 <nv.veselyi@ukma.edu.ua>
2025-11-18 21:54:34 +0200

    status 3

--- backend/users_app/models.py
+++ backend/users_app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+import logging
 
-# Create your models here.
+logger = logging.getLogger(__name__)
+
+
+class UserProfile(models.Model):
+    class Role(models.TextChoices):
+        USER = "USER", "User"
+        CHEF = "CHEF", "Chef"
+        ADMIN = "ADMIN", "Admin"
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="profile",
+    )
+    role = models.CharField(
+        max_length=10,
+        choices=Role.choices,
+        default=Role.USER,
+    )
+    bio = models.TextField(blank=True)
+    avatar_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} ({self.role})"
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            old = UserProfile.objects.get(pk=self.pk)
+            if old.role != self.role:
+                logger.info(
+                    "Role for user %s changed from %s to %s",
+                    self.user.username,
+                    old.role,
+                    self.role,
+                )
+        else:
+            logger.info(
+                "UserProfile created for user %s with role %s",
+                self.user.username,
+                self.role,
+            )
+        super().save(*args, **kwargs)
---
+++ backend/users_app/serializers.py
@@ -0,0 +1,55 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+from users_app.models import UserProfile
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ("role", "bio", "avatar_url")
+
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer(read_only=True)
+
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "profile",
+        )
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    role = serializers.ChoiceField(
+        choices=UserProfile.Role.choices,
+        default=UserProfile.Role.USER,
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+            "role",
+        )
+
+    def create(self, validated_data):
+        role = validated_data.pop("role", UserProfile.Role.USER)
+        password = validated_data.pop("password")
+
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+
+        UserProfile.objects.create(user=user, role=role)
+        return user
